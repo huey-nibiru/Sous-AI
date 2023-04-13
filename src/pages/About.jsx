6e23acaacd5fd837ce504a00d7f0ffbb642fe8fb
Yousef Macer <yousefmacer@gmail.com>
2023-04-13 14:03:36 -0400

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,5 +8,17 @@ function About() {
 		</div>
 	);
 }
+/*
+Our app is designed to cater to individuals with different dietary needs and restrictions, such as vegan, gluten-free, keto, and more. We understand that making healthy choices can be overwhelming, especially when dealing with dietary restrictions, which is why we created Sous to make it easier for you.
 
+Our cutting-edge AI algorithms consider your preferences, dietary restrictions, and nutritional goals to generate personalized meal plans and recipes that fit your lifestyle. With Sous, you no longer have to spend hours scouring the internet for recipe ideas or worrying about whether a meal meets your dietary requirements.
+
+Sous is not just a recipe app, but a comprehensive meal planning solution. Our app allows you to plan your meals for the entire week, generate a shopping list, and even track your nutrient intake. You can choose from a wide range of recipes, including breakfast, lunch, dinner, and snacks, ensuring that you never get bored with your meals.
+
+At Sous, we believe that healthy eating should be accessible to everyone. That's why we offer a range of affordable subscription plans, including a free plan that gives you access to a limited selection of recipes and meal plans.
+
+Our team of nutritionists and culinary experts is constantly updating and improving Sous, ensuring that you have access to the latest research and trends in healthy eating. With Sous, you can rest assured that you're making informed choices about what you eat, without compromising on taste or variety.
+
+Thank you for choosing Sous, and we look forward to being a part of your healthy eating journey!
+*/
 export default About;
