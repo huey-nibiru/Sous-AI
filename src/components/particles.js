e028add22547f210d41ef60471519ec30b25652c
Yousef Macer <yousefmacer@gmail.com>
2023-04-13 17:32:29 -0400

    particles

--- src/components/particles.js
+++ src/components/particles.js
@@ -2,6 +2,6 @@ import Particles from "react-tsparticles";
 import particlesConfig from "./config/particles-config.js";
 
 const ParticlesBackground = () => {
-	return <Particles></Particles>;
+	return <Particles params={particlesConfig}></Particles>;
 };
 export default ParticlesBackground;
