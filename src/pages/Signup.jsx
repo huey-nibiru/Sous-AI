48b951ae16d158b9b56a271df425f1c15fa4d0e5
Yousef Macer <yousefmacer@gmail.com>
2023-04-18 13:11:04 -0400

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,5 +1,6 @@
 import "../components/App.css";
 import Navbar from "../components/navbar";
+/*
 function Signup() {
 	return (
 		<div>
@@ -8,5 +9,61 @@ function Signup() {
 		</div>
 	);
 }
+*/
+
+import React, { useState } from 'react';
+import { auth, googleAuthProvider } from './firebase'; // import Firebase auth and Google authentication provider
+
+const SignupForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleGoogleSignup = async () => {
+    try {
+      await auth.signInWithPopup(googleAuthProvider);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Sign up</h2>
+      <form onSubmit={handleSignup}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Sign up</button>
+      </form>
+      <button onClick={handleGoogleSignup}>Sign up with Google</button>
+    </div>
+  );
+};
+
+
 
 export default Signup;
