732f87c32dd49bc14c505edd8ca686fb5ef4e2bc
Yousef Macer <yousefmacer@gmail.com>
2023-04-20 12:26:25 -0400

    Signup form

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,44 +1,63 @@
+import { React, Navbar } from "../components/dependencies";
 import { useState } from "react";
-import React from "react";
+import "../styles/signupForm.css"; // Import the CSS file for styling
+
 const SignupForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordError, setPasswordError] = useState(false);
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordError, setPasswordError] = useState(false);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+	const handleEmailChange = (e) => {
+		setEmail(e.target.value);
+	};
 
-  const handlePasswordChange = (e) => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-    const isValidPassword = passwordRegex.test(e.target.value);
-    setPassword(e.target.value);
-    setPasswordError(!isValidPassword);
-  };
+	const handlePasswordChange = (e) => {
+		const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+		const isValidPassword = passwordRegex.test(e.target.value);
+		setPassword(e.target.value);
+		setPasswordError(!isValidPassword);
+	};
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!passwordError) {
-      // Submit the form
-    }
-  };
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (!passwordError) {
+			// Submit the form
+		}
+	};
+	return (
+		<>
+			<Navbar />
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={handlePasswordChange} />
-        {passwordError && (
-          <span style={{ color: "red" }}>Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, and one number</span>
-        )}
-      </label>
-      <button type="submit">Sign up</button>
-    </form>
-  );
+			<div className="signup-form-container">
+				<form onSubmit={handleSubmit}>
+					<div className="form-group">
+						<label htmlFor="email">Email:</label>
+						<input
+							type="email"
+							id="email"
+							value={email}
+							onChange={handleEmailChange}
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="password">Password:</label>
+						<input
+							type="password"
+							id="password"
+							value={password}
+							onChange={handlePasswordChange}
+						/>
+						{passwordError && (
+							<span className="error-message">
+								Password must contain at least 8 characters, including at least
+								one uppercase letter, one lowercase letter, and one number
+							</span>
+						)}
+					</div>
+					<button type="submit">Sign up</button>
+				</form>
+			</div>
+		</>
+	);
 };
-
 export default SignupForm;
