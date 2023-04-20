af8ff0c80625411fd76c50813770c12a9d571b59
Yousef Macer <yousefmacer@gmail.com>
2023-04-20 11:56:53 -0400

    firebase update

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,68 +1,44 @@
-import {React, Navbar} from "../components/dependencies";
-/*
-function Signup() {
-	return (
-		<div>
-			<Navbar />
-			<h1>SIGN UP</h1>
-		</div>
-	);
-}
-*/
+import { useState } from "react";
+import React from "react";
+const SignupForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
 
-import { useState } from 'react';
-import { auth, googleAuthProvider } from '../services/firebase.js'; // import Firebase auth and Google authentication provider
-
-const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSignup = async (e) => {
-    e.preventDefault();
-    try {
-      await auth.createUserWithEmailAndPassword(email, password);
-      setEmail('');
-      setPassword('');
-    } catch (error) {
-      console.error(error);
-    }
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
   };
 
-  const handleGoogleSignup = async () => {
-    try {
-      await auth.signInWithPopup(googleAuthProvider);
-    } catch (error) {
-      console.error(error);
+  const handlePasswordChange = (e) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    const isValidPassword = passwordRegex.test(e.target.value);
+    setPassword(e.target.value);
+    setPasswordError(!isValidPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!passwordError) {
+      // Submit the form
     }
   };
 
   return (
-    <div>
-      <h2>Sign up</h2>
-      <form onSubmit={handleSignup}>
-        <label>
-          Email:
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <button type="submit">Sign up</button>
-      </form>
-      <button onClick={handleGoogleSignup}>Sign up with Google</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={handleEmailChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+        {passwordError && (
+          <span style={{ color: "red" }}>Password must contain at least 8 characters, including at least one uppercase letter, one lowercase letter, and one number</span>
+        )}
+      </label>
+      <button type="submit">Sign up</button>
+    </form>
   );
 };
 
-
-
-export default Signup;
+export default SignupForm;
